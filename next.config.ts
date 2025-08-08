702c1baf98e33081d0a76012018f74f8bc7b11fe
Mark Elmore <mark.elmore87@gmail.com>
2025-08-07 22:22:45 -0500

    Updated next.config.ts and package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
 
+const isGithubPages = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Required for static export
+  basePath: isGithubPages ? '/sparklepro-cleaning' : '',
+  assetPrefix: isGithubPages ? '/sparklepro-cleaning/' : '',
   reactStrictMode: true,
 };
 
